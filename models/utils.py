08126355079903826f0a22d3e8792796a9f2105c
Trung Trinh <trung.trinh@aalto.fi>
2020-07-09 22:30:13 +0300

    add partial noise

--- models/utils.py
+++ models/utils.py
@@ -139,6 +139,17 @@ class StochasticConv2d(nn.Conv2d):
                 'logstd': nn.Parameter(torch.full([height, width], init_log_std, dtype=torch.float32), requires_grad=True)
             })
             self.__noise_transform = lambda z: F.conv2d(z, self.fz.weight.abs(), None, stride, padding, dilation, groups)
+        elif noise_type == 'partial':
+            self.prior_params = nn.ParameterDict({
+                'mean': nn.Parameter(torch.full(noise_features, init_mean, dtype=torch.float32), requires_grad=False),
+                'logstd': nn.Parameter(torch.full(noise_features, init_log_std, dtype=torch.float32), requires_grad=True)
+            })
+            self.posterior_params = nn.ParameterDict({
+                'mean': nn.Parameter(torch.full(noise_features, init_mean, dtype=torch.float32), requires_grad=True),
+                'logstd': nn.Parameter(torch.full(noise_features, init_log_std, dtype=torch.float32), requires_grad=True)
+            })
+            self.fz = Linear(noise_features, out_width*out_height, False, init_method, activation)
+            self.__noise_transform = lambda z: F.linear(z, self.fz.weight.abs()).reshape((-1, 1, out_height, out_width))
         else:
             raise NotImplementedError(
                 "Currently only support full noise channel")
@@ -171,7 +182,7 @@ class StochasticConv2d(nn.Conv2d):
             dist = self.prior()
         else:
             dist = self.posterior()
-        z = dist.rsample((L, 1)) # [L, 1, H, W]
+        z = dist.rsample((L, 1)) # [L, 1, H, W] or [L, 1, n_z]
         z = self.__noise_transform(z)
         x = self.fx(x).unsqueeze_(1) + z
         return x

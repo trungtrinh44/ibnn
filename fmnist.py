dd79ebcb02676afe05cf41129b121812df563886
Trung Trinh <trung.trinh@aalto.fi>
2020-07-12 13:44:56 +0300

    update

--- fmnist.py
+++ fmnist.py
@@ -29,6 +29,7 @@ def my_config():
     weight_decay = 0.0
     lr = 1e-4
     g_min = 1.0
+    gp_weight = 1.0
     mll_iteration = 12000
     vb_iteration = 14000
     noise_type = 'full'
@@ -104,7 +105,7 @@ def test_nll(model, loader, device, num_test_sample):
 
 
 @ex.automain
-def main(_run, model_type, num_train_sample, num_test_sample, device, validate_freq, mll_iteration, vb_iteration, logging_freq, kl_weight, g_min):
+def main(_run, model_type, num_train_sample, num_test_sample, device, validate_freq, mll_iteration, vb_iteration, logging_freq, kl_weight, g_min, gp_weight):
     logger = get_logger()
     train_loader, valid_loader, test_loader = get_dataloader()
     logger.info(
@@ -157,7 +158,7 @@ def main(_run, model_type, num_train_sample, num_test_sample, device, validate_f
             by = by.to(device)
             optimizer.zero_grad()
             loglike, kl, g = model.vb_loss(bx, by, num_train_sample)
-            loss = loglike + kl_weight*kl/n_batch + torch.nn.functional.relu(g_min - g)
+            loss = loglike + kl_weight*kl/n_batch + torch.nn.functional.relu(g_min - g)*gp_weight
             loss.backward()
             optimizer.step()
             ex.log_scalar('loglike.train', loglike.item(), i)

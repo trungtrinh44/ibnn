7363d4b2b0c33bea2ef6892d176de016921cb357
Trung Trinh <trung.trinh@aalto.fi>
2020-10-12 20:26:36 +0300

    update

--- ood_test.py
+++ ood_test.py
@@ -25,6 +25,23 @@ def add_imnoise(img, mode, prob):
         imgn = (1-prob)*imgn + noise*prob
     return imgn
 
+def test_dropout(model, dataloader, device, num_test_sample, transform):
+    total_uncertainty = []
+    aleatoric_uncertainty = []
+    model.train()
+    with torch.no_grad():
+        for bx, by in dataloader:
+            bx = transform(bx).to(device)
+            by = by.to(device)
+            prob = torch.cat([
+                model.forward(bx).unsqueeze(1) for _ in range(num_test_sample)
+            ], dim=1)
+            log_prob_mean = torch.logsumexp(prob, dim=1) - torch.log(torch.tensor(prob.size(1), dtype=torch.float32, device=device))
+            total_uncertainty.append((-log_prob_mean*log_prob_mean.exp()).sum(dim=1))
+            aleatoric_uncertainty.append(
+                (-prob*prob.exp()).sum(dim=2).mean(dim=1)
+            )
+    return torch.cat(total_uncertainty, dim=0), torch.cat(aleatoric_uncertainty, dim=0)
 
 def test_stochastic(model, dataloader, device, num_test_sample, transform):
     total_uncertainty = []
@@ -68,6 +85,7 @@ if __name__ == "__main__":
     parser.add_argument('--classes', '-c', type=int, default=10)
     parser.add_argument('--batch_size', '-b', type=int, default=64)
     parser.add_argument('--ece_bins', type=int, default=15)
+    parser.add_argument('--dropout', action='store_true')
     args = parser.parse_args()
 
     device = args.device if torch.cuda.is_available() else 'cpu'
@@ -84,26 +102,57 @@ if __name__ == "__main__":
     model.load_state_dict(torch.load(checkpoint, map_location=device))
     model.to(device)
     if config['model_name'].startswith('Det'):
-        probs = np.arange(0.0, 0.55, 0.05, np.float32)
-        mean = []
-        var = []
-        for prob in probs:
-            aleatoric_uncertainty = test_model_deterministic(model, test_loader, device, lambda x: add_imnoise(x, 'salt_pepper', prob))
-            mean.append(aleatoric_uncertainty.mean().item())
-            var.append(aleatoric_uncertainty.std().item())
-        pd.DataFrame({
-            'probs': probs, 'aleatoric_mean': mean, 'aleatoric_std': var
-        }).to_csv(os.path.join(args.root, 'salt_pepper_noise.csv'), index=False)
-        probs = np.arange(0.0, 1.05, 0.1, np.float32)
-        mean = []
-        var = []
-        for prob in probs:
-            aleatoric_uncertainty = test_model_deterministic(model, test_loader, device, lambda x: add_imnoise(x, 'gaussian', prob))
-            mean.append(aleatoric_uncertainty.mean().item())
-            var.append(aleatoric_uncertainty.std().item())
-        pd.DataFrame({
-            'probs': probs, 'aleatoric_mean': mean, 'aleatoric_std': var
-        }).to_csv(os.path.join(args.root, 'gaussian_noise.csv'), index=False)
+        if args.dropout:
+            probs = np.arange(0.0, 0.55, 0.05, np.float32)
+            tm, tv, am, av, em, ev = [], [], [], [], [], []
+            for prob in probs:
+                total_uncertainty, aleatoric_uncertainty = test_dropout(model, test_loader, device, args.num_samples, lambda x: add_imnoise(x, 'salt_pepper', prob))
+                epistemic_uncertainty = total_uncertainty - aleatoric_uncertainty
+                tm.append(total_uncertainty.mean().item())
+                tv.append(total_uncertainty.std().item())
+                am.append(aleatoric_uncertainty.mean().item())
+                av.append(aleatoric_uncertainty.std().item())
+                em.append(epistemic_uncertainty.mean().item())
+                ev.append(epistemic_uncertainty.std().item())
+            pd.DataFrame({
+                'probs': probs, 'aleatoric_mean': am, 'aleatoric_std': av, 'epistemic_mean': em, 'epistemic_std': ev, 'total_mean': tm, 'total_std': tv
+            }).to_csv(os.path.join(args.root, 'dropout_salt_pepper_noise.csv'), index=False)
+            probs = np.arange(0.0, 1.05, 0.1, np.float32)
+            tm, tv, am, av, em, ev = [], [], [], [], [], []
+            for prob in probs:
+                total_uncertainty, aleatoric_uncertainty = test_dropout(model, test_loader, device, args.num_samples, lambda x: add_imnoise(x, 'gaussian', prob))
+                epistemic_uncertainty = total_uncertainty - aleatoric_uncertainty
+                tm.append(total_uncertainty.mean().item())
+                tv.append(total_uncertainty.std().item())
+                am.append(aleatoric_uncertainty.mean().item())
+                av.append(aleatoric_uncertainty.std().item())
+                em.append(epistemic_uncertainty.mean().item())
+                ev.append(epistemic_uncertainty.std().item())
+            pd.DataFrame({
+                'probs': probs, 'aleatoric_mean': am, 'aleatoric_std': av, 'epistemic_mean': em, 'epistemic_std': ev, 'total_mean': tm, 'total_std': tv
+            }).to_csv(os.path.join(args.root, 'dropout_gaussian_noise.csv'), index=False)
+        else:
+            probs = np.arange(0.0, 0.55, 0.05, np.float32)
+            mean = []
+            var = []
+            for prob in probs:
+                aleatoric_uncertainty = test_model_deterministic(model, test_loader, device, lambda x: add_imnoise(x, 'salt_pepper', prob))
+                mean.append(aleatoric_uncertainty.mean().item())
+                var.append(aleatoric_uncertainty.std().item())
+            pd.DataFrame({
+                'probs': probs, 'aleatoric_mean': mean, 'aleatoric_std': var
+            }).to_csv(os.path.join(args.root, 'salt_pepper_noise.csv'), index=False)
+            
+            probs = np.arange(0.0, 1.05, 0.1, np.float32)
+            mean = []
+            var = []
+            for prob in probs:
+                aleatoric_uncertainty = test_model_deterministic(model, test_loader, device, lambda x: add_imnoise(x, 'gaussian', prob))
+                mean.append(aleatoric_uncertainty.mean().item())
+                var.append(aleatoric_uncertainty.std().item())
+            pd.DataFrame({
+                'probs': probs, 'aleatoric_mean': mean, 'aleatoric_std': var
+            }).to_csv(os.path.join(args.root, 'gaussian_noise.csv'), index=False)
     elif config['model_name'].startswith('Sto'):
         probs = np.arange(0.0, 0.55, 0.05, np.float32)
         tm, tv, am, av, em, ev = [], [], [], [], [], []

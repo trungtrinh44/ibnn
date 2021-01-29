5ab2e556ab4de51bdf0b490f6c2d91e7430ce945
Trung Trinh <trung.trinh@aalto.fi>
2021-01-29 08:54:37 +0200

    update

--- models/resnet50_det.py
+++ models/resnet50_det.py
@@ -160,6 +160,8 @@ class ResNet(nn.Module):
                                        dilate=replace_stride_with_dilation[1])
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
+        self.bn1 = nn.BatchNorm2d(512 * block.expansion)
+        self.relu = nn.ReLU(True)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
@@ -211,7 +213,9 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
+        
+        x = self.bn1(x)
+        x = self.relu(x)
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
